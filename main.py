4c5dfd5d66898699d3c62b6f087b766547749312
artemk85 <kochetkov1985@gmail.com>
2021-05-17 13:28:10 +0300

    Подправил вывод бирки

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ for elem in xsl_data:
     name_format.set_font_size(6)
     name_format.set_align('center')
     name_format.set_align('vcenter')
-    doc_ws.merge_range('A5:J6', elem[2], name_format)
+    doc_ws.merge_range('A4:J5', elem[2], name_format)
 
     # Название товарного направления
     ntn_format = doc.add_format()
@@ -63,7 +63,7 @@ for elem in xsl_data:
     ntn_format.set_font_size(7)
     ntn_format.set_align('center')
     ntn_format.set_align('vcenter')
-    doc_ws.merge_range('A7:C7', elem[3], ntn_format)
+    doc_ws.merge_range('A6:C6', elem[3], ntn_format)
 
     # Коллекция
     koll_format = doc.add_format()
@@ -71,7 +71,7 @@ for elem in xsl_data:
     koll_format.set_font_size(7)
     koll_format.set_align('center')
     koll_format.set_align('vcenter')
-    doc_ws.merge_range('H7:J7', elem[4], koll_format)
+    doc_ws.merge_range('H6:J6', elem[4], koll_format)
 
     # Тип металла
     type_met_format = doc.add_format()
@@ -79,7 +79,7 @@ for elem in xsl_data:
     type_met_format.set_font_size(5)
     type_met_format.set_align('center')
     type_met_format.set_align('vcenter')
-    doc_ws.merge_range('A8:C8', elem[5], type_met_format)
+    doc_ws.merge_range('A7:C7', elem[5], type_met_format)
 
     # Проба
     proba_format = doc.add_format()
@@ -87,7 +87,7 @@ for elem in xsl_data:
     proba_format.set_font_size(6)
     proba_format.set_align('center')
     proba_format.set_align('vcenter')
-    doc_ws.merge_range('D8:F8', elem[6], proba_format)
+    doc_ws.merge_range('D7:F7', elem[6], proba_format)
 
     # Размер
     raz_format = doc.add_format()
@@ -95,14 +95,14 @@ for elem in xsl_data:
     raz_format.set_font_size(5)
     raz_format.set_align('center')
     raz_format.set_align('vcenter')
-    doc_ws.write('G8', 'Р-р', raz_format)
+    doc_ws.write('G7', 'Р-р', raz_format)
 
     raz1_format = doc.add_format()
     raz1_format.set_font_name('Times New Roman')
     raz1_format.set_font_size(8)
     raz1_format.set_align('center')
     raz1_format.set_align('vcenter')
-    doc_ws.merge_range('H8:J8', elem[7], raz1_format)
+    doc_ws.merge_range('H7:J7', elem[7], raz1_format)
 
     # Артикул
     art_format = doc.add_format()
@@ -110,7 +110,7 @@ for elem in xsl_data:
     art_format.set_font_size(5)
     art_format.set_align('left')
     art_format.set_align('vcenter')
-    doc_ws.merge_range('A9:J9', elem[8], art_format)
+    doc_ws.merge_range('A8:J8', elem[8], art_format)
 
     # САП код
     sap_format = doc.add_format()
@@ -118,7 +118,7 @@ for elem in xsl_data:
     sap_format.set_font_size(5)
     sap_format.set_align('left')
     sap_format.set_align('vcenter')
-    doc_ws.merge_range('A10:J10', elem[9], sap_format)
+    doc_ws.merge_range('A9:J9', elem[9], sap_format)
 
     # Цвет металла
     cvet_format = doc.add_format()
@@ -126,29 +126,31 @@ for elem in xsl_data:
     cvet_format.set_font_size(5)
     cvet_format.set_align('right')
     cvet_format.set_align('vcenter')
-    doc_ws.merge_range('A11:J11', elem[10], cvet_format)
+    doc_ws.merge_range('A10:I10', elem[10], cvet_format)
 
     # Покрытие
 
 
     # Штрихкод
     image_data = BytesIO()
-    #ean = EAN13(str(elem[12]), writer=ImageWriter())
-    #ean.write(image_data, options={"write_text": False})
+    ean = EAN13(str(elem[12]), writer=ImageWriter())
+    ean.write(image_data, options={"write_text": False})
 
-    barcode = upcean.oopfuncs.barcode('ean13', str(elem[12]))
-    filename = f'./barcodes/{str(elem[12])}.png'
-    print('CheckSum: '+str(barcode.validate_checksum()))
-    barcode.validate_create_barcode(filename, 1)
-
-    x_scale = 0.86  # *3.05
-    y_scale = 0.51  # *1.67
+    # barcode = upcean.oopfuncs.barcode('ean13', str(elem[12]))
+    # filename = f'./barcodes/{str(elem[12])}.png'
+    # print('CheckSum: '+str(barcode.validate_checksum()))
+    # barcode.validate_create_barcode(filename, 1)
+    # x_scale = 0.85  # *3.05
+    # y_scale = 0.52  # *1.67
+    x_scale = 0.191  # *13.89
+    y_scale = 0.115  # *5.39
 
     doc_ws.insert_image(
-        'A12',
-        filename,
+        'A11',
+        #filename,
+        str(elem[12]),
         {
-            #'image_data': image_data,
+            'image_data': image_data,
             'x_offset': 0,
             'y_offset': 0,
             'x_scale': x_scale,
@@ -156,50 +158,60 @@ for elem in xsl_data:
             'object_position': 0,
         }
     )
+    bc = doc.add_format()
+    bc.set_font_name('Arial')
+    bc.set_font_size(7)
+    bc.set_align('center')
+    bc.set_align('top')
+    doc_ws.merge_range('A13:J13', str(elem[12]), bc)
 
     # Цена
+    doc_ws.set_row_pixels(13, 17)
+
     cena_format = doc.add_format()
     cena_format.set_font_name('Times New Roman')
     cena_format.set_font_size(8)
     cena_format.set_align('left')
     cena_format.set_align('vcenter')
-    doc_ws.merge_range('A15:C15', 'Цена:', cena_format)
+    doc_ws.merge_range('A14:C14', 'Цена:', cena_format)
 
     cena2_format = doc.add_format()
     cena2_format.set_font_name('Times New Roman')
     cena2_format.set_font_size(10)
     cena2_format.set_align('center')
     cena2_format.set_align('vcenter')
-    doc_ws.merge_range('D15:G15', elem[13], cena2_format)
+    doc_ws.merge_range('D14:G14', elem[13], cena2_format)
 
     cena3_format = doc.add_format()
     cena3_format.set_font_name('Times New Roman')
     cena3_format.set_font_size(8)
     cena3_format.set_align('center')
     cena3_format.set_align('vcenter')
-    doc_ws.merge_range('H15:J15', 'руб.', cena3_format)
+    doc_ws.merge_range('H14:J14', 'руб.', cena3_format)
 
     # Цена за грамм
+    doc_ws.set_row_pixels(14, 9)
+
     cena_format = doc.add_format()
     cena_format.set_font_name('Times New Roman')
     cena_format.set_font_size(5)
     cena_format.set_align('right')
-    cena_format.set_align('vcenter')
-    doc_ws.merge_range('A16:C16', 'За гр.:', cena_format)
+    cena_format.set_align('top')
+    doc_ws.merge_range('A15:C15', 'За гр.:', cena_format)
 
     cena2_format = doc.add_format()
     cena2_format.set_font_name('Times New Roman')
     cena2_format.set_font_size(5)
     cena2_format.set_align('center')
-    cena2_format.set_align('vcenter')
-    doc_ws.merge_range('D16:F16', elem[14], cena2_format)
+    cena2_format.set_align('top')
+    doc_ws.merge_range('D15:F15', elem[14], cena2_format)
 
     cena3_format = doc.add_format()
     cena3_format.set_font_name('Times New Roman')
     cena3_format.set_font_size(5)
     cena3_format.set_align('left')
-    cena3_format.set_align('vcenter')
-    doc_ws.merge_range('G16:I16', 'руб.', cena3_format)
+    cena3_format.set_align('top')
+    doc_ws.merge_range('G15:I15', 'руб.', cena3_format)
 
     # Размер
     raz1_format = doc.add_format()
@@ -207,7 +219,7 @@ for elem in xsl_data:
     raz1_format.set_font_size(8)
     raz1_format.set_align('center')
     raz1_format.set_align('vcenter')
-    doc_ws.merge_range('A17:B17', elem[18], raz1_format)
+    doc_ws.merge_range('A16:B16', elem[18], raz1_format)
 
     # Цена прочерк
     cena_format = doc.add_format()
@@ -216,7 +228,7 @@ for elem in xsl_data:
     cena_format.set_font_strikeout()
     cena_format.set_align('center')
     cena_format.set_align('vcenter')
-    doc_ws.merge_range('G17:J17', f'{elem[15]}p', cena_format)
+    doc_ws.merge_range('G16:J16', f'{elem[15]}p', cena_format)
 
     # Проба
     proba_format = doc.add_format()
@@ -224,7 +236,7 @@ for elem in xsl_data:
     proba_format.set_font_size(7)
     proba_format.set_align('center')
     proba_format.set_align('vcenter')
-    doc_ws.merge_range('A18:B18', elem[19], proba_format)
+    doc_ws.merge_range('A17:B17', elem[19], proba_format)
 
     # Цена продаж
     cena_format = doc.add_format()
@@ -232,12 +244,12 @@ for elem in xsl_data:
     cena_format.set_font_size(7)
     cena_format.set_align('center')
     cena_format.set_align('vcenter')
-    doc_ws.merge_range('G18:J18', f'{elem[16]}p', cena_format)
+    doc_ws.merge_range('G17:J17', f'{elem[16]}p', cena_format)
 
     # Штрихкод
-    image_data = BytesIO()
-    ean = EAN13(str(elem[22]), writer=ImageWriter())
-    ean.write(image_data, options={"write_text": False, "includetext": True})
+    image_data2 = BytesIO()
+    ean2 = EAN13(str(elem[22]), writer=ImageWriter())
+    ean2.write(image_data2, options={"write_text": False, "includetext": True})
 
     # image_width = 143.78
     # image_height = 79.01
@@ -252,10 +264,10 @@ for elem in xsl_data:
     y_scale = 0.115  # *5.39
 
     doc_ws.insert_image(
-        'A19',
+        'A18',
         f'{elem[22]}.png',
         {
-            'image_data': image_data,
+            'image_data': image_data2,
             'x_offset': 0,
             'y_offset': 0,
             'x_scale': x_scale,
@@ -270,6 +282,6 @@ for elem in xsl_data:
     sap_format.set_font_size(8)
     sap_format.set_align('center')
     sap_format.set_align('vcenter')
-    doc_ws.merge_range('A21:J21', elem[9], sap_format)
+    doc_ws.merge_range('A20:J20', elem[9], sap_format)
 
 doc.close()
